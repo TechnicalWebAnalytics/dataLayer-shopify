0f09cfe6e7753177735a610881ece82d476fe63c
mechellewarneke <mechellewarneke@gmail.com>
2016-10-11 17:12:25 -0700

    Update dataLayer-checkout.js

--- dataLayer-checkout.js
+++ dataLayer-checkout.js
@@ -183,7 +183,7 @@ if(Shopify.Checkout.step.length > 0){
 -------------------------- */
 
 if(Shopify.Checkout.page == "thank_you"){
-	// {% if first_time_accessed %}
+	{% if first_time_accessed %}
 	dataLayer.push({
 		'transactionId': '{{checkout.order_id}}',
 		'transactionNumber': '{{checkout.order_number}}',
@@ -215,7 +215,7 @@ if(Shopify.Checkout.page == "thank_you"){
 			'pageType':'Confirmation',
 			'event':'Confirmation'
 		});
-	// {% endif %}
+	{% endif %}
 }
 
 
